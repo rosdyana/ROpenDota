5d12ff57fa70a85267efe636b3bd0d12f3c39834
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-05-03 23:08:58 +0800

    + add get_matches.R + add get_player_recent_matches.R

---
+++ R/get_matches.R
@@ -0,0 +1,21 @@
+#' Get Matches for specific player id
+#' @param account_id Steam ID
+#' @param limit matches limit
+#' @export
+#' @examples
+#' id_r3m1ck = "135474549"
+#' limit = 5
+#' matches <- get_matches(account_id = id_r3m1ck, limit = limit)
+get_matches <- function(account_id, limit){
+
+  prefix <- "https://api.opendota.com/api/players/"
+
+  url <- paste(prefix, account_id, "matches/?limit=", limit ,sep = "")
+
+  raw <- getURL(url)
+
+  matches <- fromJSON(raw)
+
+  return(matches)
+
+}
---
+++ R/get_player_recent_matches.R
@@ -0,0 +1,19 @@
+#' Get recent matches for specific player id
+#' @param account_id Steam ID
+#' @export
+#' @examples
+#' id_r3m1ck = "135474549"
+#' recentMatches <- get_recent_matches(account_id = id_r3m1ck)
+get_recent_matches <- function(account_id){
+
+  prefix <- "https://api.opendota.com/api/players/"
+
+  url <- paste(prefix, account_id, "/recentMatches",sep = "")
+
+  raw <- getURL(url)
+
+  recentMatches <- fromJSON(raw)
+
+  return(recentMatches)
+
+}
